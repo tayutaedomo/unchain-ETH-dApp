876c90a4db7b26f1ac91b0388d4690af69a96dc1
tayutaedomo <tayutaedomo@gmail.com>
2023-07-26 20:27:51 +0900

    Section 2 Lesson 1 is completed.

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
 };
