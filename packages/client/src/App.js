e518f210923b6733b81bb99f7a94bc68c6b923f8
tayutaedomo <tayutaedomo@gmail.com>
2023-07-30 16:02:03 +0900

    Section 3 Lesson 1, Small fixing

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -81,15 +81,16 @@ const App = () => {
       const { ethereum } = window;
       if (!ethereum) {
         console.log("Make sure you have MetaMask!");
+        return;
       } else {
         console.log("We have the ethereum object", ethereum);
       }
-
       const accounts = await ethereum.request({ method: "eth_accounts" });
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found");
       }
